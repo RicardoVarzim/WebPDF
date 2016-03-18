a446113852675475fcb4f9836142f7d162d87508
Ricardo Varzim <ricardovarzim@gmail.com>
2016-03-18 13:09:52 +0000

    Models Creation

---
+++ WebPDF/Models/Document.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WebPDF.Models
+{
+    public class Document
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Path { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime LastEdit { get; set; }
+        public virtual User user { get; set; }
+        
+        public virtual ICollection<Comment> Comments { get; set; }
+
+    }
+}
---
+++ WebPDF/Models/Project.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WebPDF.Models
+{
+    public class Project
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string HtmlText { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime LastEdit { get; set; }
+        public virtual User user { get; set; }
+
+        public virtual ICollection<Comment> Comments { get; set; }
+    }
+}
