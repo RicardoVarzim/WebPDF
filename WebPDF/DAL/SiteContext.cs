c15f9da5bf6e415570834677d5b52feebba66b12
Ricardo Varzim <ricardovarzim@gmail.com>
2016-03-18 13:24:36 +0000

    SiteContext

---
+++ WebPDF/DAL/SiteContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+using WebPDF.Models;
+
+namespace WebPDF.DAL
+{
+    public class SiteContext : DbContext
+    {
+        public SiteContext()
+            : base("SiteContext")
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Document> Documents { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<New> News { get; set; }
+        public DbSet<Event> Events { get; set; }
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
